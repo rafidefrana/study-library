dfbb42f50d18a48043ae4b24236c6043f4629de7
rafidefrana <rafidefrana08@gmail.com>
2024-01-09 13:49:22 +0700

    Belajar Membuat Libray

---
+++ index.js
@@ -0,0 +1,10 @@
+import {sayHello, sum} from "belajar-lbrary";
+import {max, min} from "belajar-lbrary/number";
+
+
+console.info(sayHello("Ini adalah Library yang di buat oleh Rafi dari Hasil Belajar"));
+
+console.log(sum([10, 10, 10, 10, 10]));
+
+console.log(min(10, 20));
+console.log(max(10, 20));
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+
+console.log(max(20, 2, 4, 9, 1, 100));
+console.log(min(20, 2, 4, 9, 1, -100));
